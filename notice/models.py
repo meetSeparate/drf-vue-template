eab96025f76b57b6dab237396577b0dc52688d1c
zhangziyu <1772127579@qq.com>
2023-08-07 15:05:10 +0800

    批量发送消息

--- notice/models.py
+++ notice/models.py
@@ -1,4 +1,5 @@
 from django.db import models
+from user.models import UserInfo
 
 # Create your models here.
 class Notice(models.Model):
@@ -7,6 +8,12 @@ class Notice(models.Model):
     datetime = models.DateTimeField(verbose_name='创建时间', auto_now_add=True)
     description = models.TextField(verbose_name='详细描述')
     type = models.IntegerField(verbose_name='消息类型')
+
+    user = models.ManyToManyField(
+        to=UserInfo,
+        verbose_name='对应用户',
+        null=True
+    )
 
     def __str__(self):
         return self.title
--- notice/views/message.py
+++ notice/views/message.py
@@ -10,4 +10,13 @@ class NoticeView(APIView):
             'msg': '发送成功',
             'data': {}
         }
+        user_list = request.data.get('multipleSelection')
+        notice_type = request.data.get('type')
+        avatar = request.data.get('avatar')
+        title = request.data.get('title')
+        description = request.data.get('description')
+        notice_obj = Notice.objects.create(type=notice_type, avatar=avatar, title=title, description=description)
+        for i in user_list:
+            notice_obj.user.add(i)
+        res['code'] = 200
         return Response(res)
