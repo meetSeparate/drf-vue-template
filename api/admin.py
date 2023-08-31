acd488c69347887828f46f74611d54a0ee67e849
zhangziyu <1772127579@qq.com>
2023-08-31 10:11:10 +0800

    新增商品分类和商品

--- api/admin.py
+++ api/admin.py
@@ -1,1 +1,8 @@
+from django.contrib import admin
+from category.models import Category
+from goods.models import Goods
 
+# Register your models here.
+
+admin.site.register(Category)
+admin.site.register(Goods)
