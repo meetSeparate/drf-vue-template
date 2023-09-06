1edac4de95edf6ca345b4ac39e2b9677a51648eb
zhangziyu <1772127579@qq.com>
2023-09-06 16:33:30 +0800

    商品详情

--- goods/admin.py
+++ goods/admin.py
@@ -1,3 +1,7 @@
 from django.contrib import admin
+from goods.models import GoodsSpecs, ShopDetailImage
 
 # Register your models here.
+
+admin.site.register(GoodsSpecs)
+admin.site.register(ShopDetailImage)
